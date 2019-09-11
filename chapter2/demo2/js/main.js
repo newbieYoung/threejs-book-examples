67da40704b5ef36884f16b838594b23048a285e4
Young <657851574@qq.com>
2019-09-12 01:53:55 +0800

    修改
    修改

--- chapter2/demo2/js/main.js
+++ chapter2/demo2/js/main.js
@@ -1,9 +1,61 @@
 import * as THREE from '../miniprogram_npm/three/index.js'
+const Context = canvas.getContext('webgl');
 
 /**
  * 小游戏主逻辑类
  */
 export default class Main {
   constructor() {
+    this.context = Context; //绘图上下文
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.devicePixelRatio = window.devicePixelRatio;
+    this.viewCenter = new THREE.Vector3(0, 0, 0); //原点
+
+    this.initRender();
+    this.initCamera();
+    this.initScene();
+    this.initLight();
+    this.initObject();
+    this.render();
+  }
+
+  initRender() {
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      context: this.context
+    });
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0xFFFFFF, 1.0);
+    canvas.width = this.width * this.devicePixelRatio;
+    canvas.height = this.height * this.devicePixelRatio;
+    this.renderer.setPixelRatio(this.devicePixelRatio);
+  }
+
+  initCamera() {
+    this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 1500);
+    this.camera.position.set(0, 0, 300 / this.camera.aspect);
+    this.camera.up.set(0, 1, 0); //正方向
+    this.camera.lookAt(this.viewCenter);
+
+  }
+
+  initScene() {
+    this.scene = new THREE.Scene();
+  }
+
+  initLight() {
+    this.light = new THREE.AmbientLight(0xfefefe);
+    this.scene.add(this.light);
+  }
+
+  initObject() {
+    //...
+  }
+
+  render() {
+    this.renderer.clear();
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(this.render.bind(this), canvas);
   }
 }
