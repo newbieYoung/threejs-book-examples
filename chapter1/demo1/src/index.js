18a69fb81fa03ea96695bb59a2e8aba21339f437
Young <657851574@qq.com>
2019-10-25 00:17:44 +0800

    基础程序结构
    基础程序结构

--- chapter1/demo1/src/index.js
+++ chapter1/demo1/src/index.js
@@ -2,9 +2,59 @@ import * as THREE from 'three'
 
 //应用主逻辑
 class Main {
+
   constructor() {
-    console.log('main ready');
+    this.initRender();
+    this.initScene();
+    this.initCamera();
+    this.initLight();
+    this.initObject();
+    this.render();
+  }
+
+  /**
+   * 初始化渲染器
+   */
+  initRender() {
+
+  }
+
+  /**
+   * 初始化场景
+   */
+  initScene() {
+
+  }
+
+  /**
+   * 初始化相机
+   */
+  initCamera() {
+
+  }
+
+  /**
+   * 初始化灯光
+   */
+  initLight() {
+
+  }
+
+  /**
+   * 初始化物体
+   */
+  initObject() {
+
+  }
+
+  /**
+   * 渲染
+   */
+  render() {
+
   }
 }
 
-new Main()
+window.onload = function () {
+  new Main()
+}
