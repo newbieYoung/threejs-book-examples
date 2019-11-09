d71760be94bfb0046d54aea621488876bcd6434e
Young <657851574@qq.com>
2019-11-10 00:24:09 +0800

    第一章 demo2
    第一章 demo2

---
+++ chapter1/demo2/README.md
@@ -0,0 +1,21 @@
+## 基于 npm + webpack 的 ThreeJS 浏览器基础项目
+
+## 安装
+
+在项目描述文件`package.json`所在目录依次执行以下命令：
+
+```bash
+npm install
+npx webpack
+```
+
+## 目录介绍
+
+```
+├── dist                 //webpack 构建打包生成的源代码目录
+├── src                  //基于 ES6 模块机制组织的源代码目录
+├── node_modules         //npm 下载的第三方包目录
+├── package.json         //npm 项目描述文件
+├── index.css            //样式
+└── index.html           //页面
+```
---
+++ chapter1/demo2/src/index.js
@@ -0,0 +1,51 @@
+import * as THREE from 'three'
+
+//应用主逻辑
+class Main {
+  constructor() {
+    this.initRender();
+    this.initScene();
+    this.initCamera();
+    this.initLight();
+    this.initObject();
+    this.render();
+  }
+
+  //创建渲染器
+  initRender() {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.devicePixelRatio = window.devicePixelRatio;
+
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true, //抗锯齿开启
+    });
+    this.renderer.setClearColor(0xDDDDDD, 1.0); //设置背景颜色
+    this.renderer.setSize(this.width, this.height); //设置渲染器宽度和高度
+    this.renderer.setPixelRatio(this.devicePixelRatio); //设置设备像素比
+    document.body.appendChild(this.renderer.domElement) //把画布显示到页面中
+  }
+
+  //初始化场景
+  initScene() {}
+
+  //添加相机
+  initCamera() {}
+
+  //添加灯光
+  initLight() {}
+
+  //添加物体
+  initObject() {}
+
+  //渲染成像
+  render() {
+    this.renderer.clear();
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(this.render.bind(this));
+  }
+}
+
+window.onload = function () {
+  new Main()
+}
