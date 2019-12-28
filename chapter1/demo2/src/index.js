a89979119ba324ee37db0c8183b6a9dbb8b9f736
Young <657851574@qq.com>
2019-12-28 22:16:11 +0800

    第一章 demo2 相机位置调整
    第一章 demo2 相机位置调整

--- chapter1/demo2/src/index.js
+++ chapter1/demo2/src/index.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three'
+import {
+  Mesh
+} from 'three';
 
 //应用主逻辑
 class Main {
@@ -33,18 +36,30 @@ class Main {
 
   //添加相机
   initCamera() {
-    this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 1500);
-    this.camera.lookAt(new THREE.Vector3(0, 0, 0)); // 焦点
-    this.camera.position.set(0, 0, 300 / this.camera.aspect); // 位置
+    this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 1000);
+    this.camera.position.set(200, 400, 600); // 位置
     this.camera.up.set(0, 1, 0); //上方向
     this.scene.add(this.camera);
+    this.camera.lookAt(new THREE.Vector3(0, 0, 0)); // 焦点
   }
 
   //添加灯光
-  initLight() {}
+  initLight() {
+    this.ambientLight = new THREE.AmbientLight(0xFFFFFF, 1.0); // 环境光
+    //this.scene.add(this.ambientLight);
+  }
 
   //添加物体
-  initObject() {}
+  initObject() {
+    var geometry = new THREE.BoxGeometry(100, 100, 100);
+    var material = new THREE.MeshLambertMaterial({
+      color: 0xff0000
+    });
+    var cube = new THREE.Mesh(geometry, material);
+    cube.position.set(0, 0, 0);
+    this.scene.add(cube);
+    console.log(cube.position)
+  }
 
   //渲染成像
   render() {
